3741610d6d4e78ae2e981ef5315fe5149598f266
maćkozaw <116316918+mackozaw@users.noreply.github.com>
2025-11-13 09:25:10 +0100

    urgh 3

--- parameterSweep/train_task_script.sh
+++ parameterSweep/train_task_script.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 IDX=${SLURM_ARRAY_TASK_ID}
 CSV_PATH="parameterSweep/sweep_configs.csv"
 DATA_YAML="datasets\deepscores\ds2_dense_prepared\deep_scores.yaml"
-MODEL="yolov8n.pt"
-WORKDIR=${pwd}
+MODEL="yolov8n.pt"gi
+WORKDIR=$(pwd)
 
 python parameterSweep/train_script.py \
   --csv "$WORKDIR/$CSV_PATH" \
