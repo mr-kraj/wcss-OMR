e56ccd7da7b5a5e874264d73cb355eb2370287a9
maćkozaw <116316918+mackozaw@users.noreply.github.com>
2025-11-13 09:22:39 +0100

    urgh 2

--- parameterSweep/train_task_script.sh
+++ parameterSweep/train_task_script.sh
@@ -5,12 +5,12 @@ IDX=${SLURM_ARRAY_TASK_ID}
 CSV_PATH="parameterSweep/sweep_configs.csv"
 DATA_YAML="datasets\deepscores\ds2_dense_prepared\deep_scores.yaml"
 MODEL="yolov8n.pt"
-WORKDIR=""
+WORKDIR=${pwd}
 
 python parameterSweep/train_script.py \
-  --csv "$CSV_PATH" \
+  --csv "$WORKDIR/$CSV_PATH" \
   --idx "$IDX" \
-  --data "$DATA_YAML" \
+  --data "$WORKDIR/$DATA_YAML" \
   --model "$MODEL" \
   --workdir "$WORKDIR" \
   --results_csv "$WORKDIR/parameterSweep/runs/results_${SLURM_JOB_ID}.csv"
